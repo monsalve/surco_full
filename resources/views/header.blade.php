5f37bf6d73a6081e81c23ad6248c50fa5f6aefb6
Cristhiam Monsalve <ccmonapa@hotmail.com>
2020-05-20 02:20:59 -0500

    style adjust

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,13 +1,20 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+
+      <!-- CSRF Token -->
+      <meta name="csrf-token" content="{{ csrf_token() }}">
+
       <title>Surcoestudios</title>
       
     
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
-    
+       
         <link href="https://fonts.googleapis.com/css?family=Muli:300,400,700,900" rel="stylesheet">
         <link rel="stylesheet" href="fonts/icomoon/style.css">
     
