e807d0b4955a48fc14c768b39ca28939cbf5c20e
charles <charlessm28@gmail.com>
2020-05-29 12:29:04 -0500

    Componente inscripciones
    Se crea el componente, se registra en rutas del frontEnd y se define a la barra lateral.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -51,6 +51,7 @@ let routes = [
     { path: '/empresas', component: require('./components/Empresas.vue') },
     { path: '/categorias', component: require('./components/Categorias.vue') },
     { path: '/cursos', component: require('./components/Cursos.vue') },
+    { path: '/inscripciones', component: require('./components/Inscripciones.vue') },
     { path: '*', component: require('./components/NotFound.vue') }
 ]
 
